85c636888dfdabfa8d53a80acc4bca132d0724e9
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-02 17:39:50 +0300

    feat: create component Footer

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ResponsiveAppBar from "@/components/header/Header";
+import Footer from '@/components/footer/Footer';
 
 export const App = () => {
   return (
+    <React.Fragment>
       <ResponsiveAppBar />
+      <Footer />
+    </React.Fragment>
   )
 };
