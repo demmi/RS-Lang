6f895fd326c72c0d40682a561e72883dd4954545
Andrew Gerasimchik <gerasimchick@tut.by>
2022-02-09 18:25:35 +0300

    feat: refactor Footer to MUI

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
+import React from 'react'
 
-import './Footer.css';
-import GitHubLogo from '@/assets/svg/github.svg';
-import RsSchoolLogo from '@/assets/svg/rss.svg';
-// import Avatar from '@mui/material/Avatar';
+import GitHubLogo from '@/assets/svg/github.svg'
+import RsSchoolLogo from '@/assets/svg/rss.svg'
+import { AppBar, Box, Link, Toolbar, Typography } from '@mui/material'
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="container footer-container">
-
-        <div className="footer-left">
-
-          <a className="footer-github-link"
+    <AppBar position="static" elevation={0} component="footer">
+      <Toolbar style={{ justifyContent: 'space-between' }}>
+        <Box>
+          <Link
             href="https://github.com/demmi"
-            target="_blank" rel="noreferrer">
-              <div className="author">
-                <GitHubLogo fill="#ffffff" height={20} width={20} viewBox="0 0 400 400" />
-                <div className="author-name">demmi</div>
-              </div>
-          </a>
-
-
-          <a className="footer-github-link"
+            target="_blank"
+            rel="noreferrer"
+            underline="none"
+            display="flex"
+            gap="10px"
+            style={{ alignItems: 'center' }}
+          >
+            <GitHubLogo fill="#ffffff" height={20} width={20} viewBox="0 0 400 400" />
+            <Typography variant="h6" color="white" sx={{ flexGrow: 1 }}>
+              demmi
+            </Typography>
+          </Link>
+          <Link
             href="https://github.com/sergioivanov008"
-            target="_blank" rel="noreferrer">
-              <div className="author">
-                <GitHubLogo fill="#ffffff" height={20} width={20} viewBox="0 0 400 400" />
-                <div className="author-name">sergioivanov008</div>
-              </div>
-          </a>
-        </div>
-
-        <div className="footer-center">
-          2022
-        </div>
-
-        <div className="footer-right">
-          <a className="rss-logo" href="https://rs.school/js/"
-            target="_blank" rel="noreferrer">
-            <RsSchoolLogo height={60} width={160} viewBox="0 0 242 90" />
-          </a>
-        </div>
-
-      </div>
-    </div>
-  );
+            target="_blank"
+            rel="noreferrer"
+            underline="none"
+            display="flex"
+            gap="10px"
+            style={{ alignItems: 'center' }}
+          >
+            <GitHubLogo fill="#ffffff" height={20} width={20} viewBox="0 0 400 400" />
+            <Typography variant="h6" color="white" sx={{ flexGrow: 1 }}>
+              sergioivanov008
+            </Typography>
+          </Link>
+        </Box>
+        <Typography variant="h6">© 2022</Typography>
+        <Link href="https://rs.school/js/" underline="none" target="_blank" rel="noreferrer">
+          <RsSchoolLogo fill="#ffffff" width={150} height={55} viewBox="0 0 242 90" />
+        </Link>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
-export default Footer;
+export default Footer
