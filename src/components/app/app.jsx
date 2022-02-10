5d7f546d27ef6fc77337f1cd6510d887526dd46c
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-10 16:21:31 +0300

    feat: add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,21 +2,28 @@ import React, { useState } from 'react'
 import ResponsiveAppBar from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
 import Main from '@/components/main/Main'
-import IsLogged, { FormStatus } from '@/components/context'
-import { DT_DISABLED } from '@/components/const';
+import IsLogged, { FormStatus, PageRouter } from '@/components/context'
+import { DT_DISABLED, CUR_PAGE, MAIN_PAGE } from '@/components/const';
+
+const curPage = sessionStorage.getItem(CUR_PAGE)
+  ? sessionStorage.getItem(CUR_PAGE)
+  : MAIN_PAGE;
 
 function App() {
   const [isLogged, setLogged] = useState(false)
   const [dialogType, setDialogType] = useState(DT_DISABLED)
+  const [routerPage, setRouterPage] = useState(curPage)
 
   return (
     <IsLogged.Provider value={{ isLogged, setLogged }}>
       <FormStatus.Provider value={{ dialogType, setDialogType }}>
-        <>
-          <ResponsiveAppBar />
-          <Main />
-          <Footer />
-        </>
+        <PageRouter.Provider value={{ routerPage, setRouterPage }}>
+          <>
+            <ResponsiveAppBar />
+            <Main />
+            <Footer />
+          </>
+        </PageRouter.Provider>
       </FormStatus.Provider>
     </IsLogged.Provider>
   )
--- src/components/const.jsx
+++ src/components/const.jsx
@@ -8,6 +8,20 @@ export const DT_REGISTER = 'registerForm'
 export const DT_REG_OK = 'registerOk'
 export const DT_UPDATE = 'updateForm'
 
+export const MAIN_PAGE = 'MainPage'
+export const TUTORIAL_PAGE = 'TutorialPage'
+export const GAMES_PAGE = 'GamesPage'
+export const STATISTIC_PAGE = 'StatisticPage'
+
+export const routingPages = {
+  'главная': MAIN_PAGE,
+  'учебник': TUTORIAL_PAGE,
+  'игры': GAMES_PAGE,
+  'статистика': STATISTIC_PAGE,
+}
+
+export const CUR_PAGE = 'sergioivanov008_curPage'
+
 export default Url
 
 
