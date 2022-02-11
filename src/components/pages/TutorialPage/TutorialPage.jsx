1a3c10600fff4f7df2a102eca87aad430cadf300
Andrew Gerasimchik <gerasimchick@tut.by>
2022-02-11 11:16:29 +0300

    feat: create word card

--- src/components/pages/TutorialPage/TutorialPage.jsx
+++ src/components/pages/TutorialPage/TutorialPage.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React from 'react'
 
-import './TutorialPage.css';
+import './TutorialPage.css'
+import WordCard from '@/components/pages/TutorialPage/Card/wordCard'
+import { Container, Grid } from '@mui/material'
 
 function TutorialPage() {
-    return (
-    <div className="tutorial-page">
-      <div className="main-page-title">
-        It is TutorialPage
-      </div>
-    </div>
-  );
-};
+  return (
+    <Container>
+      <Grid container spacing={{ xs: 0, md: 1 }} justifyContent="space-evenly">
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+        <WordCard />
+      </Grid>
+    </Container>
+  )
+}
 
-export default TutorialPage;
+export default TutorialPage
