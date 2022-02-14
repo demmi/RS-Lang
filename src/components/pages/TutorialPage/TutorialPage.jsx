d138d2205b33e9a2f0d1dc91ce8b19760bab5228
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-14 20:39:19 +0300

    feat: create getAllUserWord api

--- src/components/pages/TutorialPage/TutorialPage.jsx
+++ src/components/pages/TutorialPage/TutorialPage.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState, useRef, useContext } from 'react'
 
 import './TutorialPage.css'
 import WordCard from '@/components/pages/TutorialPage/Card/wordCard'
-import { Grid, ImageList, Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
 import getWords from '@/components/api/getWords'
+import getAllUserWords from '@/components/api/getAllUserWords'
+import createUserWord from '@/components/api/createUserWord'
 import { Category, Page } from '@/components/context'
 import TutorialPagination from './TutorialPagination/TutorialPagination'
 import PageOfCategories from './PageOfCategories/PageOfCategories'
+
+
 
 function TutorialPage() {
   const [words, setWords] = useState(null)
@@ -26,11 +30,42 @@ function TutorialPage() {
   useEffect(() => {
     const loadData = async () => {
       const data = await getWords(category - 1, page - 1)
-      setWords(data)
+      const userWords = await getAllUserWords(localStorage.demmiUserId, localStorage.demmiUserToken)
+
+      console.log('data:', data)
+      console.log('userWords:', userWords)
+
+      const newData = data.map((el) => {
+        const element = {...el}
+        userWords.forEach((elm) => {
+          if (elm.wordId === el.id) {
+            element.difficulty = elm.difficulty
+          }
+        })
+        return element
+      })
+
+      console.log('newData:', newData);
+
+      setWords(newData)
       setLoaded(true)
     }
     loadData()
   }, [category, page])
+
+  const setDiffWord = (curWordId) => {
+    // console.log('set diff word, word curWordId:', curWordId)
+    const setData = async () => {
+      /* const setWord = */ await createUserWord({
+        userId: localStorage.demmiUserId,
+        userToken: localStorage.demmiUserToken,
+        wordId: curWordId,
+        word: { "difficulty": "hard", "optional": {testFieldString: 'test', testFieldBoolean: true} }
+      });
+      // console.log(setWord)
+    }
+    setData()
+  }
 
   return (
     <div className="tutorial-category">
@@ -41,7 +76,7 @@ function TutorialPage() {
       <div className="empty-space"> </div>
       <div className="tutorial-content">
         <Grid container direction="row" justifyContent="center" alignItems="center" spacing={3}>
-          {loaded ? words.map(el => <WordCard data={el} key={el.id} setAudio={setAudio} />) : <h3>Loading Data</h3>}
+          {loaded ? words.map(el => <WordCard data={el} key={el.id} setAudio={setAudio} setDiffWord={setDiffWord} />) : <h3>Loading Data</h3>}
         </Grid>
       </div>
     </div>
