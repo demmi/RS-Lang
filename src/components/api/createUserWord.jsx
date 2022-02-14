dc0bd0369a137500d9de521b1cf526d1158f4a0c
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-14 22:02:55 +0300

    fix: fix word logic

--- src/components/api/createUserWord.jsx
+++ src/components/api/createUserWord.jsx
@@ -4,12 +4,14 @@ import Url from '@/components/const'
 
 async function createUserWord(id, token, wordId, diff) {
   const urlString = `${Url}users/${id}/words/${wordId}`
-  const body = { difficulty: diff, optional: {} }
+  const body = { difficulty: diff, optional: {testFieldString: 'test', testFieldBoolean: true} }
+  console.log(JSON.stringify(body))
   const response = await fetch(urlString, {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${token}`,
       Accept: 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(body),
   })
