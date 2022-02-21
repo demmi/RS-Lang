45d0ea69e14ab3fa0fb6c9f277a660e4fb09c17c
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-21 22:03:40 +0300

    feat: add statistic API

---
+++ src/components/api/statisticsPut.jsx
@@ -0,0 +1,31 @@
+import Url from '@/components/const'
+
+async function statisticsPut(id, text) {
+  const urlString = `${Url}users/${id}/statistics`
+  const body = {
+    learnedWords: 0,
+    optional: {
+      test: JSON.stringify([{'text': text}]),
+      test2: {'text': text},
+    }
+  }
+  const response = await fetch(urlString, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${localStorage.demmiUserToken}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  })
+  // if (response.ok) {
+  //   const user = await response.json()
+  //   return user
+  // } if (response.status === 401 || response.status === 400) {
+  //   const error = await response.text()
+  //   return error
+  // }
+  return response.status
+}
+
+export default statisticsPut
