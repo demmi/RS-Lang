82ca345a59ef21d47903d6e83405c973ff87f143
sergioivanov008 <sergioivanov008@gmail.com>
2022-02-23 01:33:06 +0300

    feat: add statistic logic

--- src/components/forms/FormGameRusults.jsx
+++ src/components/forms/FormGameRusults.jsx
@@ -85,7 +85,7 @@ function FormGameRusults() {
         .filter(elem => learnedWords.map(e => e.wordId).includes(elem.id) && elem.isCatch === false)
         .forEach(elem => deleteUserWord(localStorage.demmiUserId, localStorage.demmiUserToken, elem.id))
     }
-    fetchData()
+    // fetchData()
   })
 
   useEffect(() => {
@@ -99,7 +99,26 @@ function FormGameRusults() {
     const totalWord = resultsArray.length
     const numRightAnswers = resultsArray.filter((el) => el.isCatch === true).length
     const numWrongAnswers = resultsArray.filter((el) => typeof el.isCatch === 'boolean' && el.isCatch === false).length
-    const curObj = {'game': game, 'curDate': curDate, 'totalWord': totalWord, 'numRightAnswers': numRightAnswers, 'numWrongAnswers': numWrongAnswers}
+
+    let maxCatch = 0
+    let curMaxCatch = 0
+
+    resultsArray.forEach((el) => {
+      if(el.isCatch) {
+        curMaxCatch += 1
+      } else {
+        if(curMaxCatch > maxCatch) {
+          maxCatch = curMaxCatch
+        }
+        curMaxCatch = 0
+      }
+    })
+
+    if(curMaxCatch > maxCatch) {
+      maxCatch = curMaxCatch
+    }
+
+    const curObj = {'game': game, 'curDate': curDate, 'totalWord': totalWord, 'numRightAnswers': numRightAnswers, 'numWrongAnswers': numWrongAnswers, 'maxCatch': maxCatch}
     console.log('curObj:', curObj)
 
     const setStatisticData = async () => {
@@ -119,6 +138,7 @@ function FormGameRusults() {
       const learn = JSON.parse(data.optional.learned)
 
       console.log('data:', data, 'len:', count, 'callStr+:', callStr, 'sprint+:', sprint, 'learn:', learn)
+      console.log('resultsArray:', resultsArray, 'maxCatch:', maxCatch)
       await statisticsPut(localStorage.demmiUserId, localStorage.demmiUserToken, count, callStr, sprint, learn)
 
     }
